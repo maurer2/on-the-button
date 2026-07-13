806b90a6d79ae32c7b0a2477590683d0c141bd87
Martin <mr.mmaurer@gmail.com>
2026-07-13 12:00:08 +0200

    Fixed navbar anchor links

--- src/constants/nav-items-list.ts
+++ src/constants/nav-items-list.ts
@@ -6,9 +6,9 @@ type NavItem = {
 export const navItemsList = {
   code: { href: '#code', label: 'Code' },
   example: { href: '#example', label: 'Example' },
-  usage: { href: '#Usage', label: 'Usage' },
+  usage: { href: '#usage', label: 'Usage' },
   testing: { href: '#testing', label: 'Testing' },
 } as const satisfies Record<string, NavItem>;
 
 export type NavItemKey = keyof typeof navItemsList;
-export type NavItemLabel = (typeof navItemsList)[NavItemKey]['label']; // NavItem['label'] doesn't have narrowed union type
+export type NavItemLabel = (typeof navItemsList)[NavItemKey]['label']; // NavItem['label'] is widened to string instead of the union type
